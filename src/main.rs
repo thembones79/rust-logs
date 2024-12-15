bc87e495012c36b1d4460c7f7292e947711623fc
thembones79 <michal.szumnarski@gmail.com>
2024-12-15 16:32:00 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs, io::Error};
+
 fn main() {
-    println!("Hello, world!");
+    let text = fs::read_to_string("logs.txt");
+
+    println!("{:#?}", text);
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("cant divide by 0"))
+    } else {
+        Ok(a / b)
+    }
 }

54e4ce7a914a982440eaf244e0cad6916d9e621a
thembones79 <michal.szumnarski@gmail.com>
2024-12-17 20:36:18 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ fn main() {
     let text = fs::read_to_string("logs.txt");
 
     println!("{:#?}", text);
+
+    match divide(5.0, 3.0) {
+        Ok(result_of_division) => {
+            println!("{}", result_of_division)
+        }
+        Err(what_went_wrong) => {
+            println!("{}", what_went_wrong)
+        }
+    }
 }
 
 fn divide(a: f64, b: f64) -> Result<f64, Error> {

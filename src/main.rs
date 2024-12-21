faffc8294bc41cf4edf436a2907f34e2918c7c64
thembones79 <michal.szumnarski@gmail.com>
2024-12-21 16:29:08 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,39 @@ use std::{fs, io::Error};
 fn main() {
     let text = fs::read_to_string("logs.txt");
 
-    println!("{:#?}", text);
+    match text {
+        Ok(text_that_was_read) => {
+            println!("Text has {} characters.", text_that_was_read.len())
+        }
+        Err(why_this_failed) => {
+            println!("Failed to read file: {}", why_this_failed)
+        }
+    }
 
-    match divide(5.0, 3.0) {
-        Ok(result_of_division) => {
-            println!("{}", result_of_division)
-        }
-        Err(what_went_wrong) => {
-            println!("{}", what_went_wrong)
-        }
+    // println!("{:#?}", text);
+
+    // match divide(5.0, 3.0) {
+    //     Ok(result_of_division) => {
+    //         println!("{}", result_of_division)
+    //     }
+    //     Err(what_went_wrong) => {
+    //         println!("{}", what_went_wrong)
+    //     }
+    // }
+    //
+    // match validate_email(String::from("aaaa@fff.pl")) {
+    //     Ok(..) => println!("email is valid"),
+    //     Err(reason_this_failed_validation) => {
+    //         println!("{}", reason_this_failed_validation)
+    //     }
+    // }
+}
+
+fn validate_email(email: String) -> Result<(), Error> {
+    if email.contains("@") {
+        Ok(())
+    } else {
+        Err(Error::other("emails must have an @"))
     }
 }
 
